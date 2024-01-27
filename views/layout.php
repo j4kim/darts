e626e479d1b7962c9b529f74ba92ba3d69bcf5a9
Joaquim Perez <jivkim@gmail.com>
2024-01-27 23:46:16 +0100

    add htmx

--- views/layout.php
+++ views/layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./output.css" rel="stylesheet">
+    <script src="https://unpkg.com/htmx.org@1.9.10"></script>
     <title>Darts</title>
 </head>
 <body>
