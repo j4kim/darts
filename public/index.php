7e3cdd294eb6734a37b19866297af88a642ef387
Joaquim Perez <jivkim@gmail.com>
2024-01-27 22:36:34 +0100

    move logic in controller

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Bramus\Router\Router;
 use J4kim\Darts\Auth;
+use J4kim\Darts\DB;
 use League\Plates\Engine;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -11,8 +12,17 @@ session_start();
 $router = new Router();
 $templates = new Engine('../views');
 
-$router->get('/', function () use ($templates) {
-    echo $templates->render('tournament');
+$router->get('/(\d*)', function ($id) use ($templates) {
+    if (!$id) {
+        $id = DB::one("SELECT id FROM tournaments ORDER BY id DESC LIMIT 1");
+    }
+
+    $games = DB::all("SELECT * FROM games WHERE tournament_id=$id");
+
+    echo $templates->render('tournament', [
+        'games' => $games,
+        'authenticated' => Auth::check(),
+    ]);
 });
 
 $router->post('login', function () {
