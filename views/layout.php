f259cde5e1202d737a5b21df3ea8b073e7351d94
Joaquim Perez <jivkim@gmail.com>
2024-01-27 23:19:07 +0100

    new views/parts folder

--- views/layout.php
+++ views/layout.php
@@ -9,7 +9,7 @@
 <body>
     <main class="m-3">
         <div class="w-full sm:w-60">
-            <?php include(__DIR__ . "/header.php") ?>
+            <?php include(__DIR__ . "/parts/header.php") ?>
         </div>
         <?= $this->section('content') ?>
     </main>
