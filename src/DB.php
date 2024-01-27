e7af685b7192c5cb22708926e1d6ff3b1e7f2eda
Joaquim Perez <jivkim@gmail.com>
2024-01-27 21:16:12 +0100

    make DB class a singleton

--- src/DB.php
+++ src/DB.php
@@ -3,6 +3,8 @@
 namespace J4kim\Darts;
 
 class DB {
+    private static $pdoInstance;
+
     public static function connect(): \PDO
     {
         $config = require_once __DIR__ . '/../config.php';
@@ -13,5 +15,13 @@ class DB {
         $pwd = $config['DB_PWD'];
 
         return new \PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+    }
+
+    public static function pdo(): \PDO
+    {
+        if (self::$pdoInstance === null) {
+            self::$pdoInstance = self::connect();
+        }
+        return self::$pdoInstance;
     }
 }
