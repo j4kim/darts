91e388c737e007a9dcd93078af5f8d068f716253
Joaquim Perez <jivkim@gmail.com>
2024-01-27 19:58:37 +0100

    show new game button only if authenticated

--- views/games.php
+++ views/games.php
@@ -14,11 +14,16 @@ $games = $stmt->fetchAll();
 
 <h2 class="text-2xl my-4">Parties</h2>
 <div class="flex flex-col gap-2">
-    <button class="btn btn-primary" type="submit">
-        +
-    </button>
+    <?php if (@$_SESSION['username']): ?>
+        <form action="/newgame.php" method="POST">
+            <input type="hidden" name="tournament_id" value="<?= $tournamentId ?>">
+            <button class="btn btn-primary w-full" type="submit">
+                +
+            </button>
+        </form>
+    <?php endif; ?>
     <?php foreach ($games as $game): ?>
-        <a class="btn justify-start" href="/game?game=<?= $game['id'] ?>">
+        <a class="btn justify-start" href="/game.php?game=<?= $game['id'] ?>">
             <div class="w-8 opacity-50">
                 <?= $game['id'] ?>
             </div>
