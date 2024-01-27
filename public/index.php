722b4abee71cc5dcc62255703c00a20b7bce5724
Joaquim Perez <jivkim@gmail.com>
2024-01-27 20:28:33 +0100

    use bramus router

--- public/index.php
+++ public/index.php
@@ -1,21 +1,13 @@
 <?php
-    session_start();
-?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./output.css" rel="stylesheet">
-    <title>Darts</title>
-</head>
-<body>
-    <main class="m-3">
-        <div class="w-full sm:w-60">
-            <?php include("../views/header.php") ?>
-        </div>
-        <?php include("../views/games.php") ?>
-    </main>
-</body>
-</html>
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$router = new \Bramus\Router\Router();
+
+$router->get('/', function () {
+    require '../views/home.php';
+});
+
+$router->run();
---
+++ views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+    <title>Darts</title>
+</head>
+<body>
+    <main class="m-3">
+        <div class="w-full sm:w-60">
+            <?php include(__DIR__ . "/header.php") ?>
+        </div>
+        <?php include(__DIR__ . "/games.php") ?>
+    </main>
+</body>
+</html>
