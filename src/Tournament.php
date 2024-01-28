4cbca4aa5d77c099d2cf9526b194c2f5412b6fca
Joaquim Perez <jivkim@gmail.com>
2024-01-28 17:16:09 +0100

    Tournament: fetch participants usernames

--- src/Tournament.php
+++ src/Tournament.php
@@ -6,14 +6,20 @@ class Tournament
 {
     public int $id;
     public array $games = [];
+    public array $participants = [];
 
     const GETLAST = "SELECT id FROM tournaments ORDER BY id DESC LIMIT 1";
     const GETBYID = "SELECT * FROM games WHERE tournament_id=? ORDER BY date DESC";
+    const GETPARTICIPANTS = "SELECT u.username
+                             FROM tournament_participants as tp
+                             INNER JOIN users as u on tp.user_id = u.id
+                             WHERE tp.tournament_id=?";
 
     public function __construct(int $id)
     {
         $this->id = $id;
         $this->games = DB::all(self::GETBYID, [$this->id]);
+        $this->participants = DB::all(self::GETPARTICIPANTS, [$this->id]);
     }
 
     public static function getLastId()
