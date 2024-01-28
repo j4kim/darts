e8489622130557256a647758ec909f68ade32277
Joaquim Perez <jivkim@gmail.com>
2024-01-28 15:07:16 +0100

    show username in menu

--- views/parts/header.php
+++ views/parts/header.php
@@ -9,14 +9,16 @@
                 <path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
             </svg>
         </div>
-        <ul class="mt-2 p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-            <li hx-boost="true">
-                <?php if (J4kim\Darts\Auth::check()) : ?>
-                    <a href="logout">Se déconnecter</a>
-                <?php else : ?>
-                    <a href="login">Se connecter</a>
-                <?php endif; ?>
-            </li>
+        <ul 
+            hx-boost="true"
+            class="mt-2 p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52"
+        >
+            <?php if (J4kim\Darts\Auth::check()) : ?>
+                <li class="px-4 py-2 opacity-50"><?= J4kim\Darts\Auth::username() ?></li>
+                <li><a href="logout">Se déconnecter</a></li>
+            <?php else : ?>
+                <li><a href="login">Se connecter</a></li>
+            <?php endif; ?>
         </ul>
     </div>
 </header>
