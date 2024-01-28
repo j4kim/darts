1da535c886b19708bf99a5c0037dcc4466d53b26
Joaquim Perez <jivkim@gmail.com>
2024-01-28 18:06:20 +0100

    refactor: a new App class

--- public/index.php
+++ public/index.php
@@ -1,72 +1,9 @@
 <?php
 
-use Bramus\Router\Router;
-use J4kim\Darts\Auth;
-use J4kim\Darts\Game;
-use J4kim\Darts\Tournament;
-use League\Plates\Engine;
+use J4kim\Darts\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
-$router = new Router();
-$templates = new Engine('../views');
-
-$router->get('/', function () {
-    $id = Tournament::getLastId();
-    header("Location: /$id");
-});
-
-$router->get('/(\d+)', function ($id) use ($templates) {
-    echo $templates->render('tournament', [
-        'tournament' => new Tournament($id)
-    ]);
-});
-
-$router->get('/login', function () use ($templates) {
-    echo $templates->render('login');
-});
-
-$router->post('/login', function () {
-    Auth::login(...$_POST);
-    header('Location: /');
-});
-
-$router->get('/logout', function () {
-    Auth::logout();
-    header('Location: /');
-});
-
-// HTMX routes
-
-$router->get('/game/(\d*)', function ($id) use ($templates) {
-    echo $templates->render('parts/game', [
-        'game' => Game::find($id),
-    ]);
-});
-
-$echoGameEditForm = function ($id) use ($templates) {
-    echo $templates->render('parts/game.edit', [
-        'game' => Game::find($id),
-    ]);
-};
-
-$router->get('/game/(\d*)/edit', $echoGameEditForm);
-
-$router->post('/game/(\d*)', function ($id) {
-    Game::update($id, $_POST);
-    header('Location: /game/' . $id);
-});
-
-$router->post('/game/new/(\d*)', function ($tournamentId) use ($echoGameEditForm) {
-    $id = Game::create($tournamentId);
-    $echoGameEditForm($id);
-});
-
-$router->delete('/game/(\d*)', function ($id) {
-    Game::delete($id);
-    echo "<div>Partie $id supprimée</div>";
-});
-
-$router->run();
+new App();
---
+++ src/App.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace J4kim\Darts;
+
+use Bramus\Router\Router;
+use League\Plates\Engine;
+
+class App
+{
+    private Router $router;
+    private Engine $templates;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+        $this->templates = new Engine('../views');;
+        $this->registerRoutes();
+        $this->router->run();
+    }
+
+    private function registerRoutes()
+    {
+        $this->router->get('/', function () {
+            $id = Tournament::getLastId();
+            header("Location: /$id");
+        });
+
+        $this->router->get('/(\d+)', function ($id) {
+            echo $this->templates->render('tournament', [
+                'tournament' => new Tournament($id)
+            ]);
+        });
+
+        $this->router->get('/login', function () {
+            echo $this->templates->render('login');
+        });
+
+        $this->router->post('/login', function () {
+            Auth::login(...$_POST);
+            header('Location: /');
+        });
+
+        $this->router->get('/logout', function () {
+            Auth::logout();
+            header('Location: /');
+        });
+
+        // HTMX routes
+
+        $this->router->get('/game/(\d+)', function ($id) {
+            echo $this->templates->render('parts/game', [
+                'game' => Game::find($id),
+            ]);
+        });
+
+        $this->router->get('/game/(\d+)/edit', function ($id) {
+            $this->echoGameEditForm($id);
+        });
+
+        $this->router->post('/game/(\d+)', function ($id) {
+            Game::update($id, $_POST);
+            header('Location: /game/' . $id);
+        });
+
+        $this->router->post('/game/new/(\d+)', function ($tournamentId) {
+            $id = Game::create($tournamentId);
+            $this->echoGameEditForm($id);
+        });
+
+        $this->router->delete('/game/(\d+)', function ($id) {
+            Game::delete($id);
+            echo "<div>Partie $id supprimée</div>";
+        });
+    }
+
+    public function echoGameEditForm($id)
+    {
+        echo $this->templates->render('parts/game.edit', [
+            'game' => Game::find($id),
+        ]);
+    }
+}
