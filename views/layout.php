a2d39974f21effdb3af1196ed7a0e4ff5b7b9871
Joaquim Perez <jivkim@gmail.com>
2024-01-28 23:48:08 +0100

    install alpine

--- views/layout.php
+++ views/layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./output.css" rel="stylesheet">
     <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+    <script src="https://unpkg.com/alpinejs" defer></script>
     <title>Darts</title>
 </head>
 <body>
