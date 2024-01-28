a5267020317be1bd1337d8083b7a318217e59602
Joaquim Perez <jivkim@gmail.com>
2024-01-28 14:56:40 +0100

    review header for mobile screen using an icon button and a dropdown menu

--- views/parts/header.php
+++ views/parts/header.php
@@ -3,12 +3,17 @@
         <a class="text-2xl font-bold" href="/">Ptite fléchette ?</a>
     </div>
     <div class="flex-1"></div>
-    <div>
-        <ul class="menu menu-horizontal px-1">
+    <div class="dropdown dropdown-end">
+        <div class="btn btn-circle btn-ghost" tabindex="0" role="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24">
+                <path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
+            </svg>
+        </div>
+        <ul class="mt-2 p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
             <li hx-boost="true">
-                <?php if (J4kim\Darts\Auth::check()): ?>
+                <?php if (J4kim\Darts\Auth::check()) : ?>
                     <a href="logout">Se déconnecter</a>
-                <?php else: ?>
+                <?php else : ?>
                     <a href="login">Se connecter</a>
                 <?php endif; ?>
             </li>
