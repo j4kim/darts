2a98543b3e272ed55cbec7a91085cbbf58018e84
Joaquim Perez <jivkim@gmail.com>
2024-01-29 22:40:03 +0100

    abbreviated ranking header for mobile screen

--- views/parts/ranking.php
+++ views/parts/ranking.php
@@ -1,11 +1,20 @@
 <table class="table">
-    <thead>
+    <thead class="hidden sm:table-header-group">
         <tr>
             <th>Joueur</th>
             <th>Points</th>
             <th>Parties</th>
             <th>Gagnées</th>
             <th>Points par partie</th>
+        </tr>
+    </thead>
+    <thead class="sm:hidden">
+        <tr>
+            <th>Joueur</th>
+            <th>Pts</th>
+            <th>P</th>
+            <th>G</th>
+            <th>p/p</th>
         </tr>
     </thead>
     <tbody>
