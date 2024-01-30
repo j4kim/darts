7cb75bbc4c90ed98530de785ad05b871e9ba0239
Joaquim Perez <jivkim@gmail.com>
2024-01-30 23:22:52 +0100

    a reusable config function

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ use J4kim\Darts\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$config = require_once __DIR__ . '/../config.php';
+
+function config(string $key) {
+    global $config;
+    return $config[$key];
+}
+
 session_start();
 
 new App();
--- src/DB.php
+++ src/DB.php
@@ -10,12 +10,10 @@ class DB
 
     public static function connect(): PDO
     {
-        $config = require_once __DIR__ . '/../config.php';
-
-        $host = $config['DB_HOST'];
-        $db = $config['DB_NAME'];
-        $user = $config['DB_USER'];
-        $pwd = $config['DB_PWD'];
+        $host = config('DB_HOST');
+        $db = config('DB_NAME');
+        $user = config('DB_USER');
+        $pwd = config('DB_PWD');
 
         return new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
     }
