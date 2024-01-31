47b2357bb582c15f3c6d45c869f1f49054cd38a5
Joaquim Perez <jivkim@gmail.com>
2024-01-31 14:27:42 +0100

    darts favicon from svvgrepo

--- views/layout.php
+++ views/layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?= file_exists('./output.css') ? './output.css' : './dist.css' ?>" rel="stylesheet">
+    <link rel="icon" href="https://www.svgrepo.com/show/172641/darts.svg">
     <script src="https://unpkg.com/htmx.org@1.9.10"></script>
     <script src="https://unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/long-press-event/dist/long-press-event.min.js"></script>
