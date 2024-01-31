b920ba635010953e480046192c35cbc2002ac9a1
Joaquim Perez <jivkim@gmail.com>
2024-01-31 14:25:51 +0100

    use output.css (for dev) or dist.css (for prod)

--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./dist.css" rel="stylesheet">
+    <link href="<?= file_exists('./output.css') ? './output.css' : './dist.css' ?>" rel="stylesheet">
     <script src="https://unpkg.com/htmx.org@1.9.10"></script>
     <script src="https://unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/long-press-event/dist/long-press-event.min.js"></script>
