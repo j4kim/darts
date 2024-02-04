457be03e4d0d86c9388f868fed133568ea82634f
Joaquim Perez <jivkim@gmail.com>
2024-02-04 16:54:40 +0100

    test gameId cookie

--- cypress/e2e/darts.cy.js
+++ cypress/e2e/darts.cy.js
@@ -1,5 +1,12 @@
 describe('Darts', () => {
   it('successfully loads', () => {
     cy.visit('/')
+    cy.get("main > h2").contains("Liste des tournois")
+  })
+
+  it('loads preferred tournament', () => {
+    cy.setCookie('gameId', '1')
+    cy.visit('/')
+    cy.url().should('include', '/1')
   })
 })
