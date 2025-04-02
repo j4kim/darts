4b83df0d2155a0782c6880935aeb67fe11d13b79
Joaquim Perez <jivkim@gmail.com>
2025-04-02 20:35:33 +0200

    composer.json: remove password in migrate command

--- config.example.php
+++ config.example.php
@@ -4,7 +4,7 @@ return [
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'darts',
     'DB_USER' => 'root',
-    'DB_PWD' => 'sandwich',
+    'DB_PWD' => '',
     'GITHUB_HOOK_ID' => '457884403',
     'GITHUB_HOOK_REF' => 'refs/heads/master',
 ];
